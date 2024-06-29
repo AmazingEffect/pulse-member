5a0f95b81990728f992aac99256d4d982ea2f03d
wlsdks <wlsdks12@naver.com>
2024-06-29 20:42:40 +0900

    [PULM-13] :sparkles: 서비스에 빠졌던 이벤트 발행 로직 추가, 트랜잭션 추가

--- src/main/java/com/pulse/member/service/MemberService.java
+++ src/main/java/com/pulse/member/service/MemberService.java
@@ -2,26 +2,47 @@ package com.pulse.member.service;
 
 import com.pulse.member.dto.MemberDTO;
 import com.pulse.member.entity.Member;
+import com.pulse.member.event.spring.MemberCreateEvent;
 import com.pulse.member.mapper.MemberMapper;
 import com.pulse.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
+    private final ApplicationEventPublisher eventPublisher;
 
+    /**
+     * ID로 회원 조회
+     *
+     * @param id
+     * @return
+     */
     public MemberDTO getMemberById(Long id) {
         Member member = memberRepository.findById(id).orElseThrow(() -> new RuntimeException("Member not found"));
         return memberMapper.toDto(member);
     }
 
+    /**
+     * 회원 생성 + (이벤트 발행)
+     *
+     * @param memberDTO
+     * @return
+     */
+    @Transactional
     public MemberDTO createMember(MemberDTO memberDTO) {
         Member member = memberMapper.toEntity(memberDTO);
-        Member savedMember = memberRepository.save(member);
+        Member savedMember = memberRepository.saveAndFlush(member);
+
+        // MemberCreateEvent 발행
+        eventPublisher.publishEvent(new MemberCreateEvent(savedMember.getId()));
         return memberMapper.toDto(savedMember);
     }
 
