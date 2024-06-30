709b0059fd99eca04e07f98a79195fcb6379ce44
wlsdks <wlsdks12@naver.com>
2024-06-30 16:37:55 +0900

    [PULM-17] :sparkles: 서비스 수정에 따라 클래스 import 수정

--- src/main/java/com/pulse/member/controller/MemberController.java
+++ src/main/java/com/pulse/member/controller/MemberController.java
@@ -1,7 +1,7 @@
 package com.pulse.member.controller;
 
 import com.pulse.member.dto.MemberDTO;
-import com.pulse.member.service.MemberService;
+import com.pulse.member.service.usecase.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
