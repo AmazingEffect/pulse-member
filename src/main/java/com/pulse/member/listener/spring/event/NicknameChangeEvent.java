f75061973160334b7a1f525941d56cec7f7d1ca6
wlsdks <wlsdks12@naver.com>
2024-07-06 16:28:08 +0900

    [PULM-26] :sparkles: 이벤트 추가

--- src/main/java/com/pulse/member/listener/spring/event/NicknameChangeEvent.java
+++ src/main/java/com/pulse/member/listener/spring/event/NicknameChangeEvent.java
@@ -1,4 +1,4 @@
-package com.pulse.member.event.spring;
+package com.pulse.member.listener.spring.event;
 
 import com.pulse.event_library.event.OutboxEvent;
 import lombok.AllArgsConstructor;
@@ -8,13 +8,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class MemberCreateEvent implements OutboxEvent {
+public class NicknameChangeEvent implements OutboxEvent {
 
     private Long id;
 
     @Override
     public String getEventType() {
-        return "MemberCreatedOutboxEvent";
+        return "MemberNicknameChangeOutboxEvent";
     }
 
 }
