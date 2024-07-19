8ac40bd9312aa4ee69991cab76be9b3315c676bd
wlsdks <wlsdks12@naver.com>
2024-07-19 21:54:22 +0900

    [PULM-28] :sparkles: 응답코드 컨트롤러에 작성

--- src/main/java/com/pulse/member/controller/MemberController.java
+++ src/main/java/com/pulse/member/controller/MemberController.java
@@ -1,9 +1,14 @@
 package com.pulse.member.controller;
 
 import com.pulse.member.controller.request.MemberRetrieveDTO;
+import com.pulse.member.controller.response.ApiResponse;
 import com.pulse.member.service.usecase.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/member")
 @RequiredArgsConstructor
@@ -13,8 +18,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/{id}")
-    public MemberRetrieveDTO getMemberById(@PathVariable Long id) {
-        return memberService.getMemberById(id);
+    public ResponseEntity<ApiResponse<MemberRetrieveDTO>> getMemberById(@PathVariable Long id) {
+        return ResponseEntity.ok(ApiResponse.success(memberService.getMemberById(id)));
     }
 
 }
