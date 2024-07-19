52bcc2a3d7342ba1e38897cacda92d224fb5af4c
wlsdks <wlsdks12@naver.com>
2024-07-19 22:03:15 +0900

    [PULM-28] :sparkles: valid 예외 처리 로직 추가

--- src/main/java/com/pulse/member/controller/response/ApiResponse.java
+++ src/main/java/com/pulse/member/controller/response/ApiResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,17 +14,22 @@ public class ApiResponse<T> {
     private ResponseStatus success;
     private String message;
     private T data;
+    private List<String> errors;
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(ResponseStatus.SUCCESS, "Request was successful", data);
+        return new ApiResponse<>(ResponseStatus.SUCCESS, "Request was successful", data, null);
     }
 
     public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(ResponseStatus.SUCCESS, message, data);
+        return new ApiResponse<>(ResponseStatus.SUCCESS, message, data, null);
     }
 
     public static <T> ApiResponse<T> fail(String message) {
-        return new ApiResponse<>(ResponseStatus.FAIL, message, null);
+        return new ApiResponse<>(ResponseStatus.FAIL, message, null, null);
+    }
+
+    public static <T> ApiResponse<T> fail(String message, List<String> errors) {
+        return new ApiResponse<>(ResponseStatus.FAIL, message, null, errors);
     }
 
 }
--- src/main/java/com/pulse/member/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pulse/member/exception/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.pulse.member.exception;
 
 import com.pulse.member.controller.response.ApiResponse;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 전역적으로 발생하는 예외를 처리하는 클래스
@@ -18,6 +23,28 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+
+    /**
+     * Spring Boot 애플리케이션에서 데이터 유효성 검사 중에 발생하는 제약 조건 위반 예외를 처리하기 위해 사용됩니다.
+     * 이 예외는 주로 Java Bean Validation (JSR 380) 어노테이션(예: @NotNull, @Size, @Min 등)
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiResponse<String>> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
+        // 1. 제약 조건 위반 메시지를 수집합니다.
+        List<String> errors = ex.getConstraintViolations()
+                .stream()
+                .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
+                .collect(Collectors.toList());
+        
+        // 2. ApiResponse 객체를 생성하여 응답합니다.
+        ApiResponse<String> response = ApiResponse.fail("Validation error", errors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
+    /**
+     * 커스텀 예외인 MemberException을 처리하기 위해 사용됩니다.
+     */
     @ExceptionHandler(MemberException.class)
     public ResponseEntity<ApiResponse<String>> handleMemberException(MemberException e) {
         ApiResponse<String> response = ApiResponse.fail(e.getMessage());
