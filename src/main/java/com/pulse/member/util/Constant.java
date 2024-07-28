4ec9daf9ade8d63104409a7e8eb5ec4c9fbcb19f
wlsdks <wlsdks12@naver.com>
2024-07-28 17:24:07 +0900

    [PULM-28] :sparkles: activity 상수 추가

--- src/main/java/com/pulse/member/util/Constant.java
+++ src/main/java/com/pulse/member/util/Constant.java
@@ -2,9 +2,21 @@ package com.pulse.member.util;
 
 public class Constant {
 
+    // auth
     public static final String ACCESS_TOKEN = "accessToken";
     public static final String REFRESH_TOKEN = "refreshToken";
     public static final String EMAIL = "email";
     public static final String AUTHORITIES = "authorities";
+
+    // activity log
+    public static final String LOGOUT = "LOGOUT";
+    public static final String LOGIN = "LOGIN";
+    public static final String SIGN_UP = "SIGN_UP";
+    public static final String NICKNAME_CHANGE = "NICKNAME_CHANGE";
+    public static final String PASSWORD_CHANGE = "PASSWORD_CHANGE";
+    public static final String EMAIL_CHANGE = "EMAIL_CHANGE";
+    public static final String EMAIL_CERTIFICATION = "EMAIL_CERTIFICATION";
+    public static final String EMAIL_CERTIFICATION_RESEND = "EMAIL_CERTIFICATION_RESEND";
+    public static final String REISSUE_REFRESH_TOKEN = "REISSUE_REFRESH_TOKEN";
 
 }
