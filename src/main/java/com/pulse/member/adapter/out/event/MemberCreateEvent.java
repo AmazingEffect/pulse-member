90f34b7ecaef15164c3887932d9cb1c7a8af6217
wlsdks <wlsdks12@naver.com>
2024-08-02 21:57:38 +0900

    [PULM-29] :memo: 주석 설명 추가

--- src/main/java/com/pulse/member/adapter/out/event/MemberCreateEvent.java
+++ src/main/java/com/pulse/member/adapter/out/event/MemberCreateEvent.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 이벤트 발행은 외부 시스템과의 상호작용 중 하나이므로 adapter > out 이 위치에 이벤트 객체를 두는 것이 일반적이다.
+ * 또한 만약 이벤트가 내부에서만 사용된다면, domain 패키지 내에서 정의될 수도 있다.
+ * 그러나 외부 시스템과의 통신을 목적으로 한다면 adapter > out > event에 위치하는 것이 더 적절하다.
+ * 지금 나는 외부 시스템과의 통신을 목적으로 하기 때문에 adapter > out > event에 위치시킨다.
+ */
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
