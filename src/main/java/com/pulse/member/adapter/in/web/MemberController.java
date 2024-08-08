a429681d0769353a3a6798fd54a64440c364608a
wlsdks <wlsdks12@naver.com>
2024-08-08 23:14:09 +0900

    [PULM-29] :sparkles: 회원 조회 api 추가

--- src/main/java/com/pulse/member/adapter/in/web/MemberController.java
+++ src/main/java/com/pulse/member/adapter/in/web/MemberController.java
@@ -1,6 +1,13 @@
 package com.pulse.member.adapter.in.web;
 
+import com.pulse.member.adapter.in.web.dto.response.MemberResponseDTO;
+import com.pulse.member.adapter.in.web.dto.response.api.ApiResponse;
+import com.pulse.member.application.command.member.FindMemberCommand;
+import com.pulse.member.application.port.in.member.FindMemberUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 
-    // todo: 회원 정보 조회
+    private final FindMemberUseCase findMemberUseCase;
+
+    /**
+     * @param memberId 회원 ID
+     * @return 회원 정보 응답 DTO
+     * @apiNote 회원 정보 조회
+     */
+    @GetMapping("/info/{memberId}")
+    public ResponseEntity<ApiResponse<MemberResponseDTO>> getMemberInfo(
+            @PathVariable Long memberId
+    ) {
+        FindMemberCommand command = FindMemberCommand.of(memberId);
+        MemberResponseDTO responseDTO = findMemberUseCase.findMember(command);
+        return ResponseEntity.ok(ApiResponse.success(responseDTO));
+    }
 
     // todo: 유저정보 변경
 
