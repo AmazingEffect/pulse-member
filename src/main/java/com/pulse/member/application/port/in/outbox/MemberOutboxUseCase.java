4a05b1c3ee10b6729c5a5891900e55914b3665af
wlsdks <wlsdks12@naver.com>
2024-08-09 01:17:03 +0900

    [PULM-30] :sparkles: outbox 전용 port, useCase 선언

--- src/main/java/com/pulse/member/application/port/in/outbox/MemberOutboxUseCase.java
+++ src/main/java/com/pulse/member/application/port/in/outbox/MemberOutboxUseCase.java
@@ -2,16 +2,18 @@ package com.pulse.member.application.port.in.outbox;
 
 import com.pulse.member.adapter.out.event.outbox.OutboxEvent;
 
-public interface OutboxUseCase {
+public interface MemberOutboxUseCase {
 
-    void markOutboxEventProcessed(OutboxEvent event);
+    void markOutboxEventPending(OutboxEvent event);
 
-    void saveOutboxEvent(OutboxEvent event);
+    Long saveOutboxEvent(OutboxEvent event);
 
     void markOutboxEventSuccess(OutboxEvent event);
 
     void markOutboxEventFailed(OutboxEvent event);
 
     String getKafkaTopic(OutboxEvent event);
+
+    void markOutboxEventProcessed(OutboxEvent event);
 
 }
