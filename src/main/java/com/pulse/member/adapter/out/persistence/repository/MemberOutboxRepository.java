6082f2b0762418bff70df3fc593a878247153dae
wlsdks <wlsdks12@naver.com>
2024-08-09 01:17:59 +0900

    [PULM-30] :sparkles: outbox 전용 어댑터, repository 작성

--- src/main/java/com/pulse/member/adapter/out/persistence/repository/MemberOutboxRepository.java
+++ src/main/java/com/pulse/member/adapter/out/persistence/repository/MemberOutboxRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface OutboxRepository extends JpaRepository<MemberOutboxEntity, Long> {
+public interface MemberOutboxRepository extends JpaRepository<MemberOutboxEntity, Long> {
 
     Optional<MemberOutboxEntity> findByPayloadAndEventType(Long id, String eventType);
 
