1bc45c135666656cfd1cffcd821959eef8b4b036
wlsdks <wlsdks12@naver.com>
2024-11-29 16:47:22 +0900

    :recycle: 유레카 디스커버리 코드 제거

--- src/main/java/com/pulse/member/MemberApplication.java
+++ src/main/java/com/pulse/member/MemberApplication.java
@@ -2,14 +2,12 @@ package com.pulse.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MemberApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
 
 }
